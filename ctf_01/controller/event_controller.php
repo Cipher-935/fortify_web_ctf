fddead646700a80b96847316ed78d4296b7c60a8
Jay Mistry <90444018+Cipher-935@users.noreply.github.com>
2025-01-09 19:54:22 -0800

    Update event_controller.php

--- ctf_01/controller/event_controller.php
+++ ctf_01/controller/event_controller.php
@@ -2,7 +2,7 @@
 
 if($_SERVER['REQUEST_METHOD'] == "GET"){
 
-
+    // Val parameter passed in the get request will be used to access different web pages
     if(isset($_GET['val'])){
         
         $req = $_GET['val'];
@@ -21,6 +21,7 @@ if($_SERVER['REQUEST_METHOD'] == "GET"){
 
     }
 }
+// All post request handling should be done in this block
 else if($_SERVER['REQUEST_METHOD'] == "POST"){
     echo "No post yet";
 }
