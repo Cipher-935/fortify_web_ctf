d48b308a7c15743f7d68e2ba5ca4c5ce05e9f06f
AbiodaPro <azeeztriplea10@gmail.com>
2025-01-17 12:56:58 -0800

    Added upload button

--- ctf_01/views/admin_dashboard.php
+++ ctf_01/views/admin_dashboard.php
@@ -2,5 +2,6 @@
 <div class="content">  
     <h1>Admin Dashboard</h1>
     <button class='view_logs'>View Server Logs</button>
+    <button class='view_logs'>Upload</button>
 </div>
 <?php require("../views/footer.php");?>
