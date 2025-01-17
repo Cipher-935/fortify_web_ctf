5e9e019ad10d3585e4183e993df71949e027c068
AbiodaPro <azeeztriplea10@gmail.com>
2025-01-17 13:05:41 -0800

    Changed login code placement

--- ctf_01/views/homepage.php
+++ ctf_01/views/homepage.php
@@ -1,5 +1,5 @@
 <?php  require("../views/headers/header_home.php");?>
 <div class="content">  
-    <h1 hiddenDoor="rdfxeshypoi">Ctf Home</h1>
+    <h1>Ctf Home</h1>
 </div>
 <?php require("../views/footer.php");?>
