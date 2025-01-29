69ac903195647e6719ca6e7698bf3a02636f87cc
Jay Mistry <90444018+Cipher-935@users.noreply.github.com>
2025-01-28 23:55:08 -0800

    Add files via upload

--- ctf_01/controller/event_controller.php
+++ ctf_01/controller/event_controller.php
@@ -1,52 +1,54 @@
 <?php
 require('../model/event_model.php');
-if($_SERVER['REQUEST_METHOD'] == "GET"){
+if ($_SERVER['REQUEST_METHOD'] == "GET") {
 
 
-    if(isset($_GET['val'])){
-        
+    if (isset($_GET['val'])) {
+
         $req = $_GET['val'];
-        if($req == "rdfxeshypoi"){
-            require("../views/login.php");
+        if ($req == "bG9naW4gcGVyaGFwcz8/Pw==") {
+            require("../views/papaertf.php"); // Login.php
             exit();
-        }
-        else if($req == 'signup'){
-            require("../views/signup.php");
-            exit();
-        }
-       else if($req == 'home'){
+        } else if ($req == 'home') {
             require("../views/homepage.php");
             exit();
-        }
-        else if($req == 'rtyuvbgh4') {
-            require("../views/admin_dashboard.php");
+        } else if ($req == 'YWRtaW4/Pz8=') {
+            require("../views/lalaefc.php");   // Admin Panel
             exit();
-        }
-        else if($req == 'logout'){
-            session_start();
-            session_destroy();
-            require("../views/homepage.php");
+        } else if ($req == 'bWVzc2FnZXMgcHJvYmFibHk=') {
+            require("../views/mboard.php");
+            exit();
+        } else if ($req == 'eajmpu') {
+            require("../views/fileShredder.html");
+            exit();
+        } else if ($req == 'logo') {
+            require("../views/images/forum_logo.jpg");
             exit();
         }
 
     }
-}
-else if($_SERVER['REQUEST_METHOD'] == "POST"){
+} else if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $j_data = file_get_contents("php://input");
     $r_data = json_decode($j_data, true);
     $command = $r_data['command'];
-    if($command == "login"){
+    if ($command == "login") {
         $log_user = login_user($r_data['e_id'], $r_data['user_pass']);
-	echo $log_user;
+        echo $log_user;
         exit();
-	
+
+    } else if ($command == 'broadcast') {
+        $res = save_message($r_data['msg']);
+        echo $res;
+    } else if ($command == 'get_mess') {
+        $all_mess = get_messages();
+        if (empty($all_mess)) {
+            echo json_encode("No messages found yet");
+        } else {
+            echo json_encode($all_mess);
+        }
     }
-    else if($command == "view"){
-        $out = shell_exec("ls");
-        echo $out;
-    }
-    else{
-        echo 'bad';
+    else {
+        echo 'Bad Request';
     }
 }
 
